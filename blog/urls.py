00b5104b17bee9a5ded0d5b4b9e02cc41b4b152f
Jo <jomogun@gmail.com>
2017-03-19 20:21:12 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -9,4 +9,6 @@ website at the 'http://127.0.0.1:8000/' address.
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
